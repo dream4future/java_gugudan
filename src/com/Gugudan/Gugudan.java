278a65ebd5e9069bcff06fb93c259165e7e0058b
Administrator <Administrator@Fang>
2018-08-29 17:13:06 +0900

    반복 출력

--- src/com/Gugudan/Gugudan.java
+++ src/com/Gugudan/Gugudan.java
@@ -15,7 +15,7 @@ public class Gugudan {
 		}
 	}
 	public static void main(String[] args) {
-		for(int i=2; i<10; i++) {
+		for(int i=2; i<10; i++) {			//반복 출력
 			int[] result = calculate(i);
 			print(result);
 		}
